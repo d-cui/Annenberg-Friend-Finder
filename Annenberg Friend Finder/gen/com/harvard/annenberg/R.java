4e4e774555fbdc829f1ccecaab448e1043887c57
Dennis <Dennis@Dennis-PC>
2012-10-01 13:36:54 -0700

    Added log_in and sign_up

--- Annenberg Friend Finder/gen/com/harvard/annenberg/R.java
+++ Annenberg Friend Finder/gen/com/harvard/annenberg/R.java
@@ -15,10 +15,23 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int loginTitle=0x7f070001;
+        public static final int loginTitleBar=0x7f070000;
+        public static final int login_HUID=0x7f070002;
+        public static final int login_go=0x7f070004;
+        public static final int login_password=0x7f070003;
+        public static final int menu_settings=0x7f07000b;
+        public static final int signup=0x7f070005;
+        public static final int signup_HUID=0x7f070006;
+        public static final int signup_name=0x7f070007;
+        public static final int signup_password=0x7f070008;
+        public static final int signup_password_confirm=0x7f070009;
+        public static final int signup_submit=0x7f07000a;
     }
     public static final class layout {
         public static final int activity_log_in=0x7f030000;
+        public static final int log_in=0x7f030001;
+        public static final int sign_up=0x7f030002;
     }
     public static final class menu {
         public static final int activity_log_in=0x7f060000;
