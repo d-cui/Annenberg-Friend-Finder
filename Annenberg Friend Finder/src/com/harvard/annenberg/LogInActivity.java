5e76a582f9d943a290fe7b8b66602912851332d4
Marcus <Marcus@Marcus-PC>
2012-10-01 18:41:11 -0400

    Login checks database for correct password.

--- Annenberg Friend Finder/src/com/harvard/annenberg/LogInActivity.java
+++ Annenberg Friend Finder/src/com/harvard/annenberg/LogInActivity.java
@@ -2,25 +2,44 @@ package com.harvard.annenberg;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.view.View.OnClickListener;
+
 public class LogInActivity extends Activity {
+	private DbAdapter database;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		database = new DbAdapter(this);
 		setContentView(R.layout.log_in);
 		Button logInButton = (Button) findViewById(R.id.login_go);
 		logInButton.setOnClickListener(new OnClickListener() {
-			
+
 			public void onClick(View v) {
-				
-				boolean validLogin = true;
-				if(validLogin){
-					
+				int HUID = Integer
+						.parseInt(((EditText) findViewById(R.id.login_HUID))
+								.getText().toString());
+				Cursor c = database.fetchUserByHUID(HUID);
+				if (c == null) {
+					showAlert("HUID not found. Do you need to sign up?");
+					return;
 				}
+				String userPassword = c.getString(c
+						.getColumnIndexOrThrow(database.KEY_PASSWORD));
+				String enteredPassword = ((EditText) findViewById(R.id.login_password))
+						.getText().toString();
+				if (!userPassword.equals(enteredPassword)) {
+					showAlert("Incorrect password entered.");
+					return;
+				}
+				// TODO: Log-in stuff
 			}
 		});
 	}
@@ -30,4 +49,19 @@ public class LogInActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_log_in, menu);
 		return true;
 	}
+
+	private void showAlert(String text) {
+		new AlertDialog.Builder(this)
+				.setTitle("Alert")
+				.setMessage(text)
+				.setPositiveButton("Okay",
+						new DialogInterface.OnClickListener() {
+
+							public void onClick(DialogInterface dialog,
+									int which) {
+								// close
+							}
+						}).setCancelable(false).show();
+	}
+
 }
