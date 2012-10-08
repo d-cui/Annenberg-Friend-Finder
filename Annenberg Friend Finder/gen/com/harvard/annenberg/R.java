f7af37cd21516f9aafc35ba0e0eeb58cfe63da33
Dennis <Dennis@Dennis-PC>
2012-10-08 10:50:30 -0700

    updated strings and profile_layout

--- Annenberg Friend Finder/gen/com/harvard/annenberg/R.java
+++ Annenberg Friend Finder/gen/com/harvard/annenberg/R.java
@@ -8,6 +8,9 @@
 package com.harvard.annenberg;
 
 public final class R {
+    public static final class array {
+        public static final int status_array=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -20,23 +23,23 @@ public final class R {
         public static final int profile=0x7f020006;
     }
     public static final class id {
-        public static final int loginTitle=0x7f070001;
-        public static final int loginTitleBar=0x7f070000;
-        public static final int login_HUID=0x7f070002;
-        public static final int login_go=0x7f070004;
-        public static final int login_password=0x7f070003;
-        public static final int menu_settings=0x7f070010;
-        public static final int profile_HUID=0x7f070008;
-        public static final int profile_annenberg_checkbox=0x7f07000a;
-        public static final int profile_image=0x7f070006;
-        public static final int profile_in_annenberg=0x7f070009;
-        public static final int profile_name=0x7f070007;
-        public static final int signup=0x7f070005;
-        public static final int signup_HUID=0x7f07000b;
-        public static final int signup_name=0x7f07000c;
-        public static final int signup_password=0x7f07000d;
-        public static final int signup_password_confirm=0x7f07000e;
-        public static final int signup_submit=0x7f07000f;
+        public static final int loginTitle=0x7f080001;
+        public static final int loginTitleBar=0x7f080000;
+        public static final int login_HUID=0x7f080002;
+        public static final int login_go=0x7f080004;
+        public static final int login_password=0x7f080003;
+        public static final int menu_settings=0x7f080010;
+        public static final int profile_HUID=0x7f080008;
+        public static final int profile_image=0x7f080006;
+        public static final int profile_name=0x7f080007;
+        public static final int profile_status=0x7f08000a;
+        public static final int profile_status_text=0x7f080009;
+        public static final int signup=0x7f080005;
+        public static final int signup_HUID=0x7f08000b;
+        public static final int signup_name=0x7f08000c;
+        public static final int signup_password=0x7f08000d;
+        public static final int signup_password_confirm=0x7f08000e;
+        public static final int signup_submit=0x7f08000f;
     }
     public static final class layout {
         public static final int activity_log_in=0x7f030000;
@@ -49,7 +52,7 @@ public final class R {
         public static final int tab_host=0x7f030007;
     }
     public static final class menu {
-        public static final int activity_log_in=0x7f060000;
+        public static final int activity_log_in=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
@@ -58,6 +61,6 @@ public final class R {
         public static final int title_activity_log_in=0x7f040003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
